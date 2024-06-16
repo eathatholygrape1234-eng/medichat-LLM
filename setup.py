9b63e02011b4354fdcbdb895aee617a9c2368ef3
dhairya8luthra <31945980+dhairya8luthra@users.noreply.github.com>
2024-06-16 20:47:02 +0530

    index store setup done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'MediChat',
+    version= '0.0.0',
+    author= 'Dhairya Luthra',
+    author_email= 'dhairya8luthra@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
---
+++ store_index.py
@@ -0,0 +1,18 @@
+from src.helper import load_pdf, text_split, download_hugging_face_embeddings
+from dotenv import load_dotenv
+import os
+from pinecone import Pinecone
+from langchain_pinecone import PineconeVectorStore 
+
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+print(PINECONE_API_KEY)
+extracted_data = load_pdf("data/")
+text_chunks = text_split(extracted_data)
+embeddings = download_hugging_face_embeddings()
+pc = Pinecone(api_key=PINECONE_API_KEY)
+index_name="medichat"
+index = pc.Index("medichat")
+index.describe_index_stats() 
+docsearch=PineconeVectorStore.from_texts([t.page_content for t in text_chunks], embeddings, index_name=index_name)
